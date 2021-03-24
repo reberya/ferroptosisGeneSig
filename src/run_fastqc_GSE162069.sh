ca168c8896843be5f0a5df5cfbac3a178432dbd3
rebernrj <rebernrj@pathlab-ap-ps3a.med.umich.edu>
2021-03-24 16:05:23 -0400

    File to run fastqc on GSE162069 samples pre-processing

---
+++ src/run_fastqc_GSE162069.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run fastqc on samples from GSE162069
+
+fastqc -o /mctp/share/users/rebernrj/projects/ferroptosisGeneSig/int/GSE162069/fastqc \
+/mctp/share/users/rebernrj/projects/ferroptosisGeneSig/data/GSE162069/fastq/SRR13125329.fastq
+
+fastqc -o /mctp/share/users/rebernrj/projects/ferroptosisGeneSig/int/GSE162069/fastqc \
+/mctp/share/users/rebernrj/projects/ferroptosisGeneSig/data/GSE162069/fastq/SRR13125331.fastq
+
+fastqc -o /mctp/share/users/rebernrj/projects/ferroptosisGeneSig/int/GSE162069/fastqc \
+/mctp/share/users/rebernrj/projects/ferroptosisGeneSig/data/GSE162069/fastq/SRR13125333.fastq
+
+fastqc -o /mctp/share/users/rebernrj/projects/ferroptosisGeneSig/int/GSE162069/fastqc \
+/mctp/share/users/rebernrj/projects/ferroptosisGeneSig/data/GSE162069/fastq/SRR13125330.fastq
+
+fastqc -o /mctp/share/users/rebernrj/projects/ferroptosisGeneSig/int/GSE162069/fastqc \
+/mctp/share/users/rebernrj/projects/ferroptosisGeneSig/data/GSE162069/fastq/SRR13125332.fastq
+
+
+# Summarize with multiqc
+
+multiqc /mctp/share/users/rebernrj/projects/ferroptosisGeneSig/int/GSE162069/fastqc
